22c1a50f1b4e8f4d9a16c93f8d8ecce24ffcb468
Ayush K Shetty <ayush@boko.app>
2025-03-21 20:31:01 +0530

    first commit

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,14 @@
+import"../styles/LoadingIndicator.css"
+import React from 'react'
+
+const LoadingIndicator = () => {
+  return (
+    <div className="loading-container">
+        <div className="loader">
+            
+        </div>
+    </div>
+  )
+}
+
+export default LoadingIndicator
